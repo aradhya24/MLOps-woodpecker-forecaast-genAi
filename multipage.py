ca95016a404d85a859c18e73c34f87513df8c916
Manoday10 <159240524+Manoday10@users.noreply.github.com>
2024-07-23 21:11:57 +0530

    Update multipage.py

--- multipage.py
+++ multipage.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from prophet import Prophet
 from prophet.plot import add_changepoints_to_plot
 import holidays
 from datetime import date, datetime, timedelta
